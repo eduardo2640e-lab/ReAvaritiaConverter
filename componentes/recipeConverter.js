982f51415db92433722ef64cac61531e35c469e2
eduardo2640e-lab <eduardo2640e@gmail.com>
2026-07-04 03:13:49 -0300

    Create recipeConverter.js

---
+++ componentes/recipeConverter.js
@@ -0,0 +1,16 @@
+export class RecipeConverter {
+
+    constructor() {
+        this.recipes = [];
+    }
+
+    convert() {
+
+        return {
+            converted: 0,
+            skipped: 0
+        };
+
+    }
+
+}
